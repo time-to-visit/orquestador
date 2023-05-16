c3b3aac9ff30cfeebc12880d97ebb72ab7f14887
paisajegz <juan-1edinson@hotmail.es>
2023-05-16 01:35:28 -0500

    orquestador

---
+++ init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE `routes-time-to-visit` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `sites-time-to-visit` CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `user-time-to-visit` CHARACTER SET utf8 COLLATE utf8_general_ci;
+
